ebfde7f6b0424e46545b63a9b7891fea582716ac
Varun Bharathi Jayakumar <jayakumar.va@northeastern.edu>
2023-01-06 21:58:36 -0500

    finalProject Commit

--- server/index.js
+++ server/index.js
@@ -1,20 +1,23 @@
 require("dotenv").config();
-const colors = require('colors');
+const colors = require("colors");
 const express = require("express");
-const {graphqlHTTP} = require("express-graphql");
+var cors = require("cors");
+const { graphqlHTTP } = require("express-graphql");
 //used to connect GraphQl and express
 const schema = require("./schema/schema");
 const port = process.env.PORT || 5000;
 const app = express();
-const connectDB = require('./config/db');
-
+const connectDB = require("./config/db");
+app.use(cors());
 //Connect to DataBase
 connectDB();
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === 'development'
-}));
-
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
 app.listen(port, console.log(`server running on port ${port}`));
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -109,6 +109,11 @@ const myMutation = new GraphQLObjectType({
       type: ClientType,
       args: { id: { type: GraphQLNonNull(GraphQLID) } },
       resolve(parent, args) {
+        Project.find({ clientId: args.id }).then((projects) => {
+          projects.forEach((project) => {
+            project.remove();
+          });
+        });
         return Client.findByIdAndDelete(args.id);
       },
     },
